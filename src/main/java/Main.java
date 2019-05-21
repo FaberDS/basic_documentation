b0b68bdd0184bb09cb41744d21151cd5e1f609a2
Denis Schuele <schueledenis@gmx.at>
2019-05-21 22:00:50 +0200

    Integrated JUnit4 and first testmethod

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+/**
+ * @author Denis Schuele
+ */
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
+    }
+
+    /**
+     *<h2>Adding two Integer</h2>
+     *Insert to <b>Integer</b> and get as return the<i>sum</i>.
+     * @param a
+     * @param b
+     * @return The sum of a and b
+     */
+    public static int add(int a, int b){
+        return a+b;
     }
 }
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testAdd(){
+        assertEquals(3, Main.add(1,2));
+    }
+}
